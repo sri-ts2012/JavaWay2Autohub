80fd7df198957bdfc3d62f9daaad955a8502a370
bindu <sri.ts2012@gmail.com>
2020-03-02 19:24:06 +0530

    removed line

--- src/accessdemo/accessdemo1testchild.java
+++ src/accessdemo/accessdemo1testchild.java
@@ -22,7 +22,9 @@ public class accessdemo1testchild extends Accessdemotestparent   {
 				//System.out.println(obj.privateVariable);
 				System.out.println(obj.publicVariable);
 				System.out.println(obj.protectedVariable);
-				System.out.println("printing this for gitdemo");
+				//removing this as comment to test the merge conflict scenario as the original user x has this line of code
+				//System.out.println("printing this for gitdemo");
+				System.out.println("printing this from the project y workspace_NewProjects");
 			//System.out.println(obj.defaultVariable);
 			
 		
